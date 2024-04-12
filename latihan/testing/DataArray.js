b8f6adb7cf31b26bddbb8148d5eb728d70149409
JH01439X <joni.herianto@ist.id>
2024-04-12 18:53:01 +0700

    Modeify code

---
+++ latihan/testing/DataArray.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    email: "admin@email.com",
+    username: "admin",
+    password: "admin",
+  },
+  {
+    email: "karyawan@email.com",
+    username: "karyawan",
+    password: "karyawan",
+  },
+  {
+    email: "jon@email.com",
+    username: "jon",
+    password: "123456",
+  },
+];
